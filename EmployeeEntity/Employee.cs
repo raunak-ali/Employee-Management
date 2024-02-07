a9369500a12f3e514768812567a96f656bd9ee6a
raunak-ali <83573952+raunak-ali@users.noreply.github.com>
2024-02-07 12:12:56 +0530

    EmployeeEntity

---
+++ EmployeeEntity/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeEntity{
+[Serializable]
+public class Employee
+{
+ public string EmployeeID { get; set; }
+ public string EmployeeName { get; set; }
+ public string Phone { get; set; }
+}
+}
