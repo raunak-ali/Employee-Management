00b64341edda6bf16a5af6a3754d5be9223571e7
raunak-ali <83573952+raunak-ali@users.noreply.github.com>
2024-02-07 12:13:58 +0530

    HMSExeption

---
+++ HMSException/HMSException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMSException;
+
+
+
+    public class HMSException : ApplicationException
+    {
+        public HMSException() : base() { }
+
+        public HMSException(string message) : base(message) { }
+
+        public HMSException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
